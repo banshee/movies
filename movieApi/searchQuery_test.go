5509cc36a33392ed66c7a8ab481e647bf2f2e18b
James Moore <james@restphone.com>
2023-02-07 18:56:32 -0800

    Adding tests

--- movieApi/searchQuery_test.go
+++ movieApi/searchQuery_test.go
@@ -8,20 +8,24 @@ import (
 	"testing"
 )
 
+// Test to see that a search returns a Batman movie
 func TestSearch(t *testing.T) {
 	server := NewServer(os.Getenv("APIKEY"), "www.omdbapi.com")
 	url, err := url.Parse("http://localhost:8080/?s=Batman&page=2")
 	assert.Nil(t, err)
 	result, err := server.Search(url)
 	assert.Nil(t, err)
+	// Could use a more specific test here
 	assert.True(t, strings.Contains(result, "Batman: The Killing Joke"))
 }
 
+// Test that a detail request returns details for the right movie
 func TestDetail(t *testing.T) {
 	server := NewServer(os.Getenv("APIKEY"), "www.omdbapi.com")
 	url, err := url.Parse("http://localhost:8080/detail/tt1569923")
 	assert.Nil(t, err)
 	result, err := server.Detail(url)
 	assert.Nil(t, err)
+	// Could use a more specific test here
 	assert.True(t, strings.Contains(result, "Brandon Vietti"))
 }
