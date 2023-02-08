6c4ca07f52147e39f8f91d851fab6990d9a1a2b0
James Moore <james@restphone.com>
2023-02-07 18:52:37 -0800

    Adding tests

--- movieApi/searchQuery_test.go
+++ movieApi/searchQuery_test.go
@@ -1,11 +1,27 @@
 package movieApi
 
 import (
+	"github.com/stretchr/testify/assert"
+	"net/url"
+	"os"
+	"strings"
 	"testing"
 )
 
 func TestSearch(t *testing.T) {
+	server := NewServer(os.Getenv("APIKEY"), "www.omdbapi.com")
+	url, err := url.Parse("http://localhost:8080/?s=Batman&page=2")
+	assert.Nil(t, err)
+	result, err := server.Search(url)
+	assert.Nil(t, err)
+	assert.True(t, strings.Contains(result, "Batman: The Killing Joke"))
 }
 
-func TestConfiguration_ToUrl(t *testing.T) {
+func TestDetail(t *testing.T) {
+	server := NewServer(os.Getenv("APIKEY"), "www.omdbapi.com")
+	url, err := url.Parse("http://localhost:8080/detail/tt1569923")
+	assert.Nil(t, err)
+	result, err := server.Detail(url)
+	assert.Nil(t, err)
+	assert.True(t, strings.Contains(result, "Brandon Vietti"))
 }
